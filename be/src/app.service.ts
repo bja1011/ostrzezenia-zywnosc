3ea3656855902c0a304db16ab1e61b246c652c2e
adam <kaczmarek@oskarwegner.pl>
2019-05-06 12:46:12 +0200

    NestJS controller and service to extract data from GIS website until they fix api dataset to be updated.

--- be/src/app.service.ts
+++ be/src/app.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import * as cheerio from 'cheerio';
+import * as md5 from 'md5';
+import { BASE_POSTS_URL } from './constants';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import FIREBASE_CONFIG from './firebase.config';
 
 @Injectable()
 export class AppService {
+  db: firebase.firestore.Firestore;
 
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService,) {
+    firebase.initializeApp(FIREBASE_CONFIG);
+    this.db = firebase.firestore();
+  }
+
+  /**
+   * Extract data from GIS website until they fix api dataset to be updated.
+   */
+  async extractPostsFromHtml() {
+    const resp = await this.httpService.get(BASE_POSTS_URL)
+      .toPromise();
+    const c = cheerio.load(resp.data);
+    const pagesCount = parseInt(c('.pagination .page-numbers').not('.next').last().html());
+    this.getPagePosts(1);
+    for (let page = 2; page <= pagesCount; page++) {
+      await this.getPagePosts(page);
+    }
+  }
+
+  /**
+   * Put extracted data into firebase database.
+   * @param pageNumber
+   */
+  async getPagePosts(pageNumber: number = 1) {
+    const pageUrl = pageNumber ? `${BASE_POSTS_URL}page/${pageNumber}/` : BASE_POSTS_URL;
+    const pageHtml = await this.httpService.get(pageUrl)
+      .toPromise();
+    const c = cheerio.load(pageHtml.data);
+    const links = c('.category-list__item').toArray();
+    links.forEach(linkContainer => {
+      const link = c(linkContainer).find('.layer-link').attr('href');
+      const title = c(linkContainer).find('.category-list__title').text();
+      const image = c(linkContainer).find('.category-list__image img').attr('src');
+
+      this.db.collection('posts').doc(md5(link)).set({
+        url: link,
+        title: title,
+        image: image ? image : null,
+      });
+    });
   }
 }
