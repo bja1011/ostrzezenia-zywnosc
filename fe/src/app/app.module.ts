386907c3ef2e6e23a9a09676196e9d4630583648
adam <kaczmarek@oskarwegner.pl>
2019-05-14 17:19:50 +0200

    Ngrx store and schematics added.

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataProviderModule } from './modules/data-provider/data-provider.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './store/reducers';
+import { AppEffects } from './store/effects/app.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +24,10 @@ import { DataProviderModule } from './modules/data-provider/data-provider.module
     IonicModule.forRoot(),
     AppRoutingModule,
     DataProviderModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [
     StatusBar,
